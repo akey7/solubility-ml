6f33df4b498429ec25cabcf8b2edad9d32edaa76
Alicia Key <alicia@akey7.com>
2020-05-25 14:00:03 -0600

    Issue #3
    Preprocess the data for the algorithms.

--- binary-preprocess.R
+++ binary-preprocess.R
@@ -7,6 +7,13 @@ library(dplyr)
 original <- read.csv("data/delaney-original.csv")
 originalTib <- as_tibble(original)
 
+# Drop the ESOL calculated column, be
+
 mutated <- originalTib %>%
+  select(-ESOL.predicted.log.solubility.in.mols.per.litre) %>%
+  select(-smiles) %>%
   mutate(solubility.above.or.below.median = 
            ifelse(measured.log.solubility.in.mols.per.litre <= medianSolubility, 0, 1))
+
+# Write the transformed tibble as a .csv
+write_csv(mutated, "data/delaney-binary.csv")
